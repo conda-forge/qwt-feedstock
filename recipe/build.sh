07b402ee950a8693163b7fb89274342a4a5ae68d
Julien Schueller <schueller@phimeca.com>
2025-12-22 11:48:31 +0100

    cross

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,9 +12,6 @@ which g++
 qmake6 ../qwt.pro
 
 make -j${CPU_COUNT}
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
-make check
-fi
 make install
 
 # No test suite, but we can build examples in "examples/" as a check.
